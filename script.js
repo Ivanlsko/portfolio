7ee45eab896a4543b9324c1024ee13565a7ecf9f
Ivnlesko <il.ivanlesko@example.com>
2021-05-10 16:33:37 +0200

    JS simplified

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 const navItem = document.querySelectorAll(".navItem");
 const navItemArray = Array.from(navItem);
 
-navItemArray[0].addEventListener("mouseenter", hoverStateNav);
-navItemArray[1].addEventListener("mouseenter", hoverStateNav);
-navItemArray[2].addEventListener("mouseenter", hoverStateNav);
-navItemArray[3].addEventListener("mouseenter", hoverStateNav);
-navItemArray[4].addEventListener("mouseenter", hoverStateNav);
-navItemArray[5].addEventListener("mouseenter", hoverStateNav);
-
-navItemArray[0].addEventListener("mouseleave", hoverStateNav);
-navItemArray[1].addEventListener("mouseleave", hoverStateNav);
-navItemArray[2].addEventListener("mouseleave", hoverStateNav);
-navItemArray[3].addEventListener("mouseleave", hoverStateNav);
-navItemArray[4].addEventListener("mouseleave", hoverStateNav);
-navItemArray[5].addEventListener("mouseleave", hoverStateNav);
+navItem.forEach((a) => {
+  a.addEventListener("mouseenter", hoverStateNav);
+  a.addEventListener("mouseleave", hoverStateNav);
+});
 
 function hoverStateNav() {
   console.log("hello");
@@ -25,15 +16,10 @@ const button = document.querySelectorAll(".button");
 const buttonArray = Array.from(button);
 console.log(buttonArray);
 
-buttonArray[0].addEventListener("mouseenter", buttonHover);
-buttonArray[1].addEventListener("mouseenter", buttonHover);
-buttonArray[2].addEventListener("mouseenter", buttonHover);
-buttonArray[3].addEventListener("mouseenter", buttonHover);
-
-buttonArray[0].addEventListener("mouseleave", buttonHover);
-buttonArray[1].addEventListener("mouseleave", buttonHover);
-buttonArray[2].addEventListener("mouseleave", buttonHover);
-buttonArray[3].addEventListener("mouseleave", buttonHover);
+buttonArray.forEach((button) => {
+  button.addEventListener("mouseenter", buttonHover);
+  button.addEventListener("mouseleave", buttonHover);
+});
 
 function buttonHover() {
   this.classList.toggle("bigger");
